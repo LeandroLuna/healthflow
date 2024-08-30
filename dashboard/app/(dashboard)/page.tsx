c5eebefa7b870a9ddf4b9fb77797707ebbd337c4
Leandro Luna <leandro.j.luna@gmail.com>
2024-08-29 21:58:36 -0300

    feat: add dashboard files

---
+++ dashboard/app/(dashboard)/page.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Typography from '@mui/material/Typography';
+import { auth } from '../../auth';
+
+export default async function HomePage() {
+  const session = await auth();
+
+  return (
+    <Typography>
+      Bem-vindo ao dashboard, {session?.user?.name || 'User'}!
+    </Typography>
+  );
+}
---
+++ dashboard/app/layout.tsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import { AppProvider } from '@toolpad/core/nextjs';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import MapIcon from '@mui/icons-material/Map';
+import type { Navigation } from '@toolpad/core';
+import { SessionProvider, signIn, signOut } from 'next-auth/react';
+import { auth } from '../auth';
+import theme from '../theme';
+
+const NAVIGATION: Navigation = [
+  {
+    kind: 'header',
+    title: 'Itens do menu',
+  },
+  {
+    segment: '',
+    title: 'Dashboard',
+    icon: <DashboardIcon />,
+  },
+  {
+    segment: 'map',
+    title: 'Mapa',
+    icon: <MapIcon />,
+  },
+];
+
+const BRANDING = {
+  logo: <img src="https://healthflow.netlify.app/_astro/HealthflowLogo.C15_rad7.png" style={{paddingRight: '10px'}} alt='HealthFlow Logo'/>,
+  title: 'HealthFlow',
+};
+
+const AUTHENTICATION = {
+  signIn,
+  signOut,
+};
+
+export default async function RootLayout(props: { children: React.ReactNode }) {
+  const session = await auth();
+
+  return (
+    <html lang="en">
+      <header>
+        <title>HealthFlow</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </header>
+      <body>
+        <SessionProvider session={session}>
+          <AppRouterCacheProvider options={{ enableCssLayer: true }}>
+            <AppProvider
+              navigation={NAVIGATION}
+              branding={BRANDING}
+              session={session}
+              authentication={AUTHENTICATION}
+              theme={theme}
+            >
+              {props.children}
+            </AppProvider>
+          </AppRouterCacheProvider>
+        </SessionProvider>
+      </body>
+    </html>
+  );
+}
