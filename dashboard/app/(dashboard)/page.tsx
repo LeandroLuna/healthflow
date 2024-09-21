964015327c8b4ab6280a2597b392e1d6df71353f
Leandro Luna <leandro.j.luna@gmail.com>
2024-09-21 16:34:15 -0300

    fix: dont hidden initial page title

--- dashboard/app/(dashboard)/page.tsx
+++ dashboard/app/(dashboard)/page.tsx
@@ -3,7 +3,7 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { auth } from '../../auth';
-import { Alert } from '@mui/material';
+import { Alert, Paper } from '@mui/material';
 
 export default async function HomePage() {
   const session = await auth();
@@ -12,9 +12,9 @@ export default async function HomePage() {
 
   return (
     <>
-      <Typography>
-        Bem-vindo ao dashboard{session?.user?.name ? ', ' + session.user.name + '!' : '!'}
-      </Typography>
+      <Paper elevation={2} sx={{ margin: 2, padding: 2, borderRadius: 2, backgroundColor: '#023f81' }}>
+        <Typography variant="h4" sx={{ fontFamily: 'opensanshebrewcondensed-regular', textTransform: 'uppercase', fontWeight: 'bold', textAlign: 'center', color: 'whitesmoke' }}>Dashboard</Typography>
+      </Paper>
       <Box sx={{ marginTop: 4 }}>
         <Alert severity="info" sx={{ marginBottom: 2, textAlign: 'center' }}>
           Exemplo demonstrativo dos widgets dos gráficos. Os widgets reais ainda estão em desenvolvimento.
